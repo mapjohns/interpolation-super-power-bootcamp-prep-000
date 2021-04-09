906856ecd41de4be9c9a3b0649cefd62709329f5
Matthew Johnson <mattj20@gmail.com>
2021-04-09 16:51:39 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,16 @@
 # Write your #display_rainbow method here
+def display_rainbow(color)
+  new = Array.new
+  i = 0
+  while i < color.length() do
+    first_l = color[i][0].upcase
+    test1 = "#{first_l}: #{color[i]}"
+    new.append(test1)
+    i += 1
+  end
+  puts new
+  puts new.length()
+end
+
+display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+
